27e49d5cbcf9eb8525c13130f6c490e0d1fc66d2
JS <justinsbits@gmail.com>
2021-12-16 17:15:19 -0800

    add Header component

--- App.js
+++ App.js
@@ -1,29 +1,19 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <View style={styles.header}>
-      <Header
-        style={styles.headerTitle}
-        title="Lets build another simple app"
-      />
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
+      <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  header: {
-    width: "100%",
-    height: 90,
-    paddingTop: 36,
-    backgroundColor: "#f7287b",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  headerTitle: {
-    color: "black",
-    fontSize: 18,
+  screen: {
+    flex: 1,
   },
 });
