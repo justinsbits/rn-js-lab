d2d4c8a943fc56ba6c0133d028aca6ad07ab5857
JS <justinsbits@gmail.com>
2021-12-29 15:36:28 -0800

    implement baseline categorymealsscreen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,20 +1,51 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+import { Header } from "react-native/Libraries/NewAppScreen";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { MEALS } from "../data/dummy-data";
+import CustomHeaderButton from "../components/HeaderButton";
 
-const MealDetailScreen = props => {
+const MealDetailScreen = (props) => {
+  const mealId = props.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
   return (
     <View style={styles.screen}>
-      <Text>The Meal Detail Screen!</Text>
+      <Text>{selectedMeal.title}</Text>
+      <Button
+        title="Go Back to Categories"
+        onPress={() => {
+          props.navigation.popToTop();
+        }}
+      />
     </View>
   );
+};
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam("mealId");
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  return {
+    headerTitle: selectedMeal.title,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("Mark as favorite!");
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
 
 export default MealDetailScreen;
