86135cd65fc41d60fdc1ff59f7089ec4310108fd
JS <justinsbits@gmail.com>
2021-12-30 16:57:49 -0800

    favorite screen updated to use meallist comp

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,24 +1,16 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import MealList from "../components/MealList";
+import { MEALS } from "../data/dummy-data";
 
 const FavoritesScreen = (props) => {
-  return (
-    <View style={styles.screen}>
-      <Text>The Favorites Screen!</Text>
-    </View>
+  const dummyFavMeals = MEALS.filter(
+    (meal) => meal.id == "m1" || meal.id == "m2"
   );
+  return <MealList listData={dummyFavMeals} navigation={props.navigation} />;
 };
 
 export const favoritesScreenOptions = {
   headerTitle: "Your Favorites",
 };
-
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
 
 export default FavoritesScreen;
