25c2f70391aad3ced0d3295410db928f41d0a8b0
JS <justinsbits@gmail.com>
2021-12-31 14:04:03 -0800

    config to use redux

--- App.js
+++ App.js
@@ -4,9 +4,18 @@ import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 import { enableScreens } from "react-native-screens";
 
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
 import AppNavigator from "./navigation/AppNavigator";
+import mealsReducer from "./store/reducers/meals";
 
 enableScreens(); // improved perf for larger apps -  configure react-navigation to use screens instead of plain RN Views for rendering screen views
+
+// supports merging various reducers
+const rootReducer = combineReducers({
+  meals: mealsReducer,
+});
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -28,5 +37,10 @@ export default function App() {
     );
   }
 
-  return <AppNavigator />;
+  // wrap all components interacting with store with provider
+  return (
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
+  );
 }
