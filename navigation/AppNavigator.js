6999dee7b37c79c97e06b6a4428772113c6a7a5d
JS <justinsbits@gmail.com>
2022-01-05 16:41:06 -0800

    navigate based on authentication state

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,18 @@
+import { useSelector } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
-import { MainMealsDrawerNavigator } from "../navigation/MealsNavigator";
+import {
+  MainMealsDrawerNavigator,
+  AuthNavigator,
+} from "../navigation/MealsNavigator";
+
+import StartupScreen from "../screens/Startup";
 
 const AppNavigator = (props) => {
+  const isAuthenticated = useSelector((state) => !!state.auth.token);
   return (
     <NavigationContainer>
-      <MainMealsDrawerNavigator />
+      {isAuthenticated && <MainMealsDrawerNavigator />}
+      {!isAuthenticated && <AuthNavigator />}
     </NavigationContainer>
   );
 };
